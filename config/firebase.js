0c3e1632a87a9d08ee07a85633fbdee8c685cdc8
saadwali1082 <saadwalimuhammad1082@gmail.com>
2026-07-16 15:03:30 +0500

    Updated firebase.js to use environment variables
    Refactored Firebase initialization to use environment variables for service account credentials instead of a JSON file.

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,27 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getAuth } = require("firebase-admin/auth");
-const { getFirestore } = require("firebase-admin/firestore");
+// config/firebase.js
+const admin = require("firebase-admin");
 
-const serviceAccount = require("../study-genie-84c70-firebase-adminsdk-fbsvc-f1482245d4.json");
+// Use environment variables instead of JSON file
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CERT_URL,
+  universe_domain: "googleapis.com"
+};
 
-const app = initializeApp({
-  credential: cert(serviceAccount),
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
-const auth = getAuth(app);
-const db = getFirestore(app);
+const auth = admin.auth();
+const db = admin.firestore();
 
 module.exports = {
   auth,
